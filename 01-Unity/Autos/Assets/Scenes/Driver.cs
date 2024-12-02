41a33fd5fb67d92082872499a69d0c7747c26eeb
Joel Guingla <luis.guingla@epn.edu.ec>
2024-12-01 22:50:43 -0500

    (Class 3,4 and 5) Add new scripts and update project settings for collision and following behavior

--- 01-Unity/Autos/Assets/Scenes/Driver.cs
+++ 01-Unity/Autos/Assets/Scenes/Driver.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
+    [SerializeField] float steerSpeed = 100f;
+    [SerializeField] float moveSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 0, 45);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount); //se mueve en cada tick
+        transform.Translate(0, moveAmount, 0); //se mueve en cada tick
     }
 }
